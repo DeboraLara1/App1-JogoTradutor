910feea355a5ec3faad51fbf8f12aaa0601bae46
Debora Lara <debora.roldanlara@gmail.com>
2020-05-25 00:35:00 -0300

    subindo atualizacoes responsivas no componente painel

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FRASES } from './frases-mock'
+import { Frases } from '../shered/frase.model';
 
 @Component({
   selector: 'app-painel',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  public frases: Frases[] = FRASES
+  public instrucao: string = "Traduza a frase:"
+  public resposta: string
+
+
+  constructor() {
+    console.log (this.frases)
+   }
 
   ngOnInit(): void {
+  }
+  //Retorna o valor digitado no html, com auxilio do <HTMLInputElement> é possivel
+  // chamar o value que se encontra dentro do script, sem ele não conseguimos pois declara erro((<HTMLInputElement>resposta.target).value)
+  // se faz necessario isolar a resposta.target para poder pegar o value
+  public atualizaResposta(resposta: Event):void{
+    this.resposta = ((<HTMLInputElement>resposta.target).value)
+    console.log(this.resposta)
   }
 
 }
