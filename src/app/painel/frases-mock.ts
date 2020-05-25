85aeb572da1d58ecdfe1fb415618326fd614b864
Debora Lara <debora.roldanlara@gmail.com>
2020-05-25 01:29:47 -0300

    subindo component  Model

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,9 @@
+import { Frases } from '../shered/frase.model'
+
+export const FRASES: Frases[] = [
+    {fraseEng: 'I love to learn', frasePtBr: 'Eu gosto de aprender'},
+    {fraseEng: 'I watch tv', frasePtBr: 'Eu assito TV'},
+    {fraseEng: 'How are you', frasePtBr: 'Como vai você?'},
+    {fraseEng: 'I eat bread', frasePtBr: 'Eu como pão'},
+
+]
